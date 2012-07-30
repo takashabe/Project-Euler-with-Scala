b105a76bebdb91408d264b433b7868937c3c0c2b
takashabe <takashabe.public@gmail.com>
2012-07-30 23:13:44 +0900

    test case add from sample

--- src/test/scala/TestProblem1.scala
+++ src/test/scala/TestProblem1.scala
@@ -4,6 +4,9 @@ import org.specs2.mutable._
 
 object TestProblem1 extends Specification {
   "Problem1" should {
+    "10未満の3か5の倍数になっている数字の合計は23" in {
+      Problem1.sumMultiplesOf2NumsBelow(3, 5, 9) must_== 23
+    }
     "1000未満の3か5の倍数になっている数字の合計" in {
       Problem1.sumMultiplesOf2NumsBelow(3, 5, 1000) must_== 234168
     }
