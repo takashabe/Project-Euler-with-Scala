014ba96ccc2260bfa753281eb867f9f2c47b2e62
takashabe <takashabe.public@gmail.com>
2012-08-04 22:07:27 +0900

    answered problem2

--- src/main/scala/com/takashabe/pj_euler/scala/Problem2.scala
+++ src/main/scala/com/takashabe/pj_euler/scala/Problem2.scala
@@ -1,14 +1,15 @@
 package pj_euler
 
 object Problem2 extends App{
-  def sumFibonacciEven(n: Int): Int = {
-    var list = List(1, 0)
-    for(i <- 1 to n) {
-      val tmp = list.head + list.tail.head
-      if(tmp < n)
-        list = tmp :: list
+  def sumFibonacciEven(n1: Int, n2: Int, max: Int): Int = {
+    fibList(n1, n2, max) filter(_ % 2 == 0) sum
+  }
+
+  def fibList(n1: Int, n2:Int, max:Int): List[Int] = {
+    val n = n1 + n2
+    n match {
+      case n if n > max => Nil
+      case _           => n :: fibList(n2, n, max)
     }
-    println(list)
-    list sum
   }
 }
--- src/test/scala/TestProblem2.scala
+++ src/test/scala/TestProblem2.scala
@@ -4,8 +4,12 @@ import org.specs2.mutable._
 
 object TestProblem2 extends Specification {
   "Problem2" should {
-    "フィボナッチ数列の8以下の項の合計" in {
-      Problem2.sumFibonacciEven(8) must_== 9
+    "フィボナッチ数列の項の値が8以下である範囲の偶数値の総和" in {
+      Problem2.sumFibonacciEven(0, 1, 8) must_== 10
+    }
+
+    "フィボナッチ数列の項の値が400万以下である範囲の偶数値の総和" in {
+      Problem2.sumFibonacciEven(0, 1, 4000000) must_== 4613732
     }
   }
 }
